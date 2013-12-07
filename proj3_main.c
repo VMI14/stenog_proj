7918a939241b59ac6d5d5d7511f470eedbc13013
VMI14 <tommy.bylund@gmail.com>
2013-12-06 21:38:47 -0500

    Update proj3_main.c

--- proj3_main.c
+++ proj3_main.c
@@ -1,177 +1,88 @@
 /*
- * This program extracts a hidden file from a provided ppm file.
- *
- * Author: Tommy Bylund and Mike Mayhew
- * Date: 12/1/2013
- */
- 
+* This program extracts a hidden file from a provided ppm file.
+*
+* Author: Tommy Bylund and Mike Mayhew
+* Date: 12/1/2013
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-// Define some constants for the available operations
-#define READ      1
-#define WRITE     2
-#define INVERSE   3
-
-#define EXIT      8
-
-// function prototypes for the PPM I/O functions
-void readPPMImage( char name[], unsigned char image[][]);
-void writePPMImage( char name[], unsigned char image[][]);
-
-// function prototypes for the image processing functions
-// YOU NEED TO SUPPLY IMPLEMENTATIONS OF ALL OF THESE! See below.
-int getMenuSelection(void);
-void inverse( unsigned char image[][]);
-
-
 // main -- print menu, get user's selection, and process it.
-int main(void)
+int main(int argc, char *argv[])
 {
-   unsigned char image[][];  // array to hold image data
-   int choice;                      	// menu choice from user
-   char filename[80];               	// filename entered by user
-   
-   do {
-      // print menu and get user's selection
-      choice = getMenuSelection();
-      
-      // returned value should be between READ and EXIT
-      assert( choice >= READ && choice <= EXIT );
-      
-      // process the selection
-      switch (choice) {
-         case READ:     // get filename from the user
-                        printf("Enter the filename to read: ");
-                        scanf("%79s", filename);
-                        readPPMImage( filename, image );
-                        break;
-                        
-         case WRITE:    // get filename from the user
-                        printf("Enter the filename to write: ");
-                        scanf("%79s", filename);
-                        writePPMImage( filename, image );
-                        break;
-                        
-         case INVERSE:  inverse( image );
-                        break;
-                
-         default:       // do nothing
-                        break;
-      }
-   } while ( choice != EXIT );      // repeat until they exit
-   
-   return 0;
-}
-/*
- * readPPMImage -- 
- */
-void readPPMImage( char name[], unsigned char image[][])
- {
-   FILE *fp;
+   char filename[80];         // filename entered by user
+  
+   FILE *fp_read;
+   FILE *fp_write;
    int i, j;
    int width, height, depth;
-
-   // open the file
-   fp = fopen( name, "r" );
-   if ( fp == NULL ) {
-      fprintf(stderr, "ERROR -- Cannot open input file %s\n", name);
+   int b, size, data;
+     
+   //open the file STEP 1
+   if( (argc < 3)){
+     fprintf(stderr, "ERROR -- argc must be 3%s --invalid .ppm file\n", argv[1]);
+     exit(1); //make sure that the user enters enough into the command line
+   } 
+   
+   fp_read = fopen( argv[1], "r" );
+   if ( fp_read == NULL ) {
+      fprintf(stderr, "ERROR -- Cannot open input file %s\n", argv[1]);
       exit(1);
    }
 
-   // read PPM header stuff
-   if ( fscanf(fp, "P6 %d%d%d\n", &width, &height, &depth) != 3 ) {
-      fprintf(stderr, "ERROR -- Cannot read input file %s -- invalid .ppm file\n",
-                 name);
+   // open the file
+   fp_write = fopen( argv[2], "w" ); //argv[1]
+   if( fp_write == NULL ) {
+      fprintf(stderr, "ERROR -- Cannot open output file %s\n", argv[2]);
+      exit(1);
+   }
+
+   // read PPM header stuff STEP 2
+   if ( fscanf(fp_read, "P6 %d%d%d\n", &width, &height, &depth) != 3 ) {
+      fprintf(stderr, "ERROR -- Cannot read input file %s -- invalid .ppm file\n", argv[1]);
       exit(1); //read the PPM header info
    }
 
-   for (i = 0; i < height; i++) {
-      for (j = 0; j < width; j++) {
-         if ( feof( fp ) ) {
-            fprintf(stderr, "ERROR -- Invalid input file %s.\n", name);
-            exit(1);
-         }
-         else {
-            image[i][j][0] = getc(fp); //read the pixel data /red
-	    image[i][j][1] = getc(fp); //read the pixel data /green
-	    image[i][j][1] = getc(fp); //read the pixel data /blue
-         }
-      }
-   }
-   // close file
-   fclose( fp );
-}
-/*
- * writePPMImage -- 
- */
-void writePPMImage( char name[], unsigned char image[][])
-{
-   int i, j;
-   int width, height, depth;
-   FILE *fp;
-
-   // open the file
-   fp = fopen( name, "w" );
-   if( fp == NULL ) {
-      fprintf(stderr, "ERROR -- Cannot open output file %s\n", name);
+   //Check to see if file is valid. width & height > 0 and depth == 255
+   if ( (width < 0) || (height < 0) || (depth != 255) ) {
+      fprintf(stderr, "ERROR -- Cannot read input file %s -- unsupported .ppm file\n",
+		argv[1]);
       exit(1);
    }
-
-   // write PPM header
-   fprintf(fp, "P6\nheight width\ndepth\n", height, width, depth);
-
-   // write image data
-   for ( i = 0; i < height; i++ ) {
-      for ( j = 0; j < width; j++ ) {
-         putc( image[i][j], fp );
-      }
-   }
-
-   // close the file
-   fclose( fp );
-}
-        
-// Stub functions so that the program can be compiled. Be sure to include the real implementations of all of these functions.
-
-/*
- * Print the menu, get the menu choice from the user, and return it.
- * Input: none
- * Returns menu selection from user (a number from 1 to 8).
- */
-int getMenuSelection(void)
-{
-   int getMenuOption;
    
-   printf("Welcome to image Stenographer\n");
-   printf("Enter 1 to read the program.\n");
-   printf("Choices:\n 3. Inverse\n");
-   printf("Enter 2 to write the program.\n");
-   printf("Enter 8 to exit.\n");
-     
-     scanf("%d", &getMenuOption);
- 
-   return getMenuOption;
+   //write the output file STEP 3 and 4
+   
+   // write image data
+   
+   //use get()C to read a byte from file ... b = c&1; extracts LSB... size = size(size<<1)
+      
+   for (i=0; i < 32; i++) 
+   {      
+     b = getc(fp_read)&1; //lsb
+     size = (size<<1)|b; //add bit to size
+   }
+   
+   
+   printf("size of file %d \n", size); 
+   for (i=0; i <size; i++)
+   {
+
+   data = 0;
+   
+     for( j= 0; j<8; j++)
+     {
+     b = getc(fp_read)&1; //lsb
+     data = (data<<1)|b; 
+     }
+
+   putc(data, fp_write);
+
+   }
+       
+   // close file
+   fclose( fp_read );
+   fclose( fp_write );
+   
+   return 0;
 }
-/*
- * Take the inverse of an image.
- * Input: image = array holding the image data, i and j input variables, i represents the row of the selected pixel and j represents the column. 
- 
- * Returns nothing, but modifies the image array
- */
-
-
-
-void inverse( unsigned char image[256][256] )
-
-{
-int i, j;
-	
-	for(i=0; i<256; i++){
-	   for (j=0; j<256; j++) {
-   	      image[i][j] = 255-image[i][j];
-   	   }
-   	}
-}
-
